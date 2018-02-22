5b7a04b8af6407b22c38f5bcf23fb9192d27fe45
Adam Towers <hello@adamtowers.design>
2018-02-22 01:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
   } else {
     var itemsInCart = [];
     for (var i=0; i < cart.length; i++) {
-    itemsInCart.push(`${Object.keys(cart[0][i])} at $${Object.values(cart[0])}`)
+    itemsInCart.push(`${Object.keys(cart[0][i])} at $${Object.values(cart[0][i])}`)
     }
     return console.log(`In your cart, you have ${itemsInCart.join(`, `)}.`)
   }
